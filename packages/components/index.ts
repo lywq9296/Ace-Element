834f75bf6d95e95fe14bd05270d4f8e2acbe9088
lywq9296 <ywli0314@sina.com>
2025-03-11 07:09:08 +0800

    chore: 调整 @Vue3-Element/theme 子包目录结构

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -4,6 +4,8 @@ import type { App } from 'vue';
 import components from './components';
 import type { Install } from './types';
 
+import '@Vue3-Element/theme';
+
 export const install: Install<App<Element>> = (app) => {
   if (install.installed) {
     return;
