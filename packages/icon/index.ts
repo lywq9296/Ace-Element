f2f0e9b1fedefbca6fcb37730ac9b84fac6ae141
lywq9296 <ywli0314@sina.com>
2025-03-16 20:45:08 +0800

    feat:  图标库子包初始化

---
+++ packages/icon/index.ts
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+
+export { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faUserSecret);
