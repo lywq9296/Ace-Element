1c8b10ec86a1027802801c807d5d472fb4c37263
lywq9296 <ywli0314@sina.com>
2025-03-21 06:24:51 +0800

    test: button 组件 disabled, click  测试用例

--- packages/components/Button/__test__/Button.test.ts
+++ packages/components/Button/__test__/Button.test.ts
@@ -1,5 +1,6 @@
-import { describe, test } from 'vitest';
+import { describe, test, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
+
 import Button from '../src/index.vue';
 
 describe('Button.vue', () => {
@@ -14,5 +15,37 @@ describe('Button.vue', () => {
     });
 
     console.log(wrapper.html());
+    // class
+    expect(wrapper.classes()).toContain('ace-button--primary');
+
+    // slot
+    // 获取元素:get, find
+    expect(wrapper.get('button').text()).toBe('button');
+
+    // 事件
+    wrapper.get('button').trigger('click');
+    console.log(wrapper.emitted());
+    expect(wrapper.emitted()).toHaveProperty('click');
+  });
+
+  test('disabled button', () => {
+    const wrapper = mount(Button, {
+      props: {
+        type: 'primary',
+        disabled: true,
+      },
+      slots: {
+        default: 'disabled button',
+      },
+    });
+    console.log(wrapper.html());
+
+    // attributes
+    expect(wrapper.attributes('disabled')).toBeDefined();
+    expect(wrapper.find('button').element.disabled).toBeDefined();
+
+    // 事件
+    wrapper.get('button').trigger('click');
+    expect(wrapper.emitted()).not.toHaveProperty('click');
   });
 });
